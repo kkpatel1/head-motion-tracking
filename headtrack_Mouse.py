12298e8829ab1ef28e6d1a0d152d5d36665a54b8
Kartik Patel <kartikp1995@yahoo.co.in>
2015-03-12 01:47:49 +0530

    Final Code added

--- headtrack_Mouse.py
+++ headtrack_Mouse.py
@@ -5,7 +5,9 @@ import pyautogui
 
 HAAR_CASCADE_PATH = "haarcascade_frontalface_alt.xml"
 CAMERA_INDEX = 0
-DISTURBANCE_TOLERANCE = 20  #Sensitivity
+DISTURBANCE_TOLERANCE_HORI = 20.0  #Sensitivity
+DISTURBANCE_TOLERANCE_UP = 15.0  #Sensitivity
+DISTURBANCE_TOLERANCE_DOWN = -10.0
 CAMERA_FPS = 20
 
 def detect_faces(image):
@@ -21,34 +23,43 @@ def get_motion(faceCenter, originCenter):
 	#[0][0] - x, [0][1] - y, [0][2] - w, [0][3] - h
 	horizontal_change = faceCenter[0] - originCenter[0]
 	vertical_change = faceCenter[1] - originCenter[1]
-	if abs(horizontal_change) < DISTURBANCE_TOLERANCE and abs(vertical_change) < DISTURBANCE_TOLERANCE:
-		print 'ORIGIN'
+	
+	ver = vertical_change < DISTURBANCE_TOLERANCE_UP and vertical_change > DISTURBANCE_TOLERANCE_DOWN
+	if abs(horizontal_change) < DISTURBANCE_TOLERANCE_HORI and ver:
+		print 'ORIGIN', horizontal_change, vertical_change 
 		return 25, 0
-	if abs(horizontal_change) > abs(vertical_change):
-		if horizontal_change > 0:
-			print 'LEFT'
-			return 1, horizontal_change
+	if vertical_change >= 0:
+		if abs(horizontal_change) > (DISTURBANCE_TOLERANCE_HORI/abs(DISTURBANCE_TOLERANCE_DOWN))*abs(vertical_change):
+			if horizontal_change > 0:
+				print 'LEFT', horizontal_change, vertical_change
+				return 1, horizontal_change
+			else:
+				print 'RIGHT', horizontal_change, vertical_change 
+				return 0, -horizontal_change
 		else:
-			print 'RIGHT'
-			return 0, -horizontal_change
-	if abs(horizontal_change) < abs(vertical_change):
-		if vertical_change > 0:
-			print 'DOWN'
+			print 'DOWN', horizontal_change, vertical_change 
 			return 2, vertical_change
-		else:
-			print 'UP'
-			return 3, -vertical_change
-	else:
-#		print horizontal_change, vertical_change
-		return 25, 0
 
+	if vertical_change < 0:
+		if abs(horizontal_change) > (DISTURBANCE_TOLERANCE_HORI/abs(DISTURBANCE_TOLERANCE_UP))*abs(vertical_change):
+			if horizontal_change > 0:
+				print 'LEFT', horizontal_change, vertical_change 
+				return 1, horizontal_change
+			else:
+				print 'RIGHT', horizontal_change, vertical_change 
+				return 0, -horizontal_change
+		else:
+			print 'UP', horizontal_change, vertical_change 
+			return 3, vertical_change
+	print horizontal_change, vertical_change
 
 def run():
     """ Create a pygame screen until it is closed. """
     running = True
     i = 0
     originCenter = None
-    faceArray = [] 
+    faceArray = []
+    faceCenter = None
     while running:
      	retval, image = capture.read()
 
@@ -59,8 +70,8 @@ def run():
 			faceCenter = (faces[0][0]+faces[0][2]/2, faces[0][1]+faces[0][3]/2)
        		print 'current coords',faces
 
-	if i<=10:
-		faceArray.append(faceCenter)
+		if faceCenter:
+			faceArray.append(faceCenter)
 
        	for (x,y,w,h) in faces:
        		cv2.cv.Rectangle(cv2.cv.fromarray(image), (x,y), (x+w,y+h), 255)
@@ -73,20 +84,19 @@ def run():
       		if direction in key_to_function:
       			key_to_function[direction]()
 
-       	if i==10 and faces:
+       	if i%20==0 and faces:
        		#approx 3 secs of config time
 		faceCenter_avg_y = sum([j[1] for j in faceArray])/len(faceArray)
 		faceCenter_avg_x = sum([j[0] for j in faceArray])/len(faceArray)
 		originCenter = [faceCenter_avg_x, faceCenter_avg_y]
-		faceArray = None
+		faceArray = []
       		print 'origin is ',originCenter
-       	
 
 	cv2.imshow("Video",image)
 	if not faces:
 		i -= 1
        	i += 1
-       	c = cv2.waitKey(5)
+       	c = cv2.waitKey(1)
 
        	if c==27:
        		break
